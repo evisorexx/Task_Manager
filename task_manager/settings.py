050639854ffe6c83b185bd559684826e321d6cfe
Andrey M <tolkoigri@yandex.ru>
2024-11-30 09:02:26 +0300

    Fix for db #7.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -113,9 +113,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 if os.getenv('DATABASE_URL'):
-    db_from_env = dj_database_url.config(conn_max_age=600)
-    DATABASES['default'].update(db_from_env)
+    env_db = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(env_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
