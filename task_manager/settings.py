5210f813988cde7bca4ceaf4cd2b62a8cf7da7f0
Andrey M <tolkoigri@yandex.ru>
2024-11-30 04:50:00 +0300

    Fix db.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -102,16 +102,16 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if local_config.get('ENVIRON', None) == 'dev':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-if local_config.get('ENVIRON', None) == 'prod':
-    DATABASES = {'default': dj_database_url.config()}
+}
 
+if os.getenv('DATABASE_URL'):
+    db = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
